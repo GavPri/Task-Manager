d86c0572a722b83a00986d6b487f5d8191dae5b5
GavPri <gavinprior5@gmail.com>
2023-07-26 14:16:15 +0000

    add task list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Task
 # Create your views here.
+
+
+def get_task_list(request):
+    tasks = Task.objects.all()
+    context = {
+        'task': task
+    }
+    return render(request, 'tasks.html', context)
