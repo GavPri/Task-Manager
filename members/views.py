ff27419e4a1e211c01f270049cb8642b9090ca76
GavPri <>
2023-07-26 22:27:31 +0000

    def login user

--- members/views.py
+++ members/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
+
+# View to login user
+def login_user(request):
