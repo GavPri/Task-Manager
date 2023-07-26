abb5ab3cbcea2834109a27bf12b335839da3ba5d
GavPri <>
2023-07-26 22:51:13 +0000

    add to login view

--- members/views.py
+++ members/views.py
@@ -3,7 +3,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
 
+
 # View to login user
 def login_user(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        return redirect('home')
+        else:
+            # Message for unsuccessfull login
+            messages.success{request, ("Login Error, Try Again.")}
+        pass
+    else:
+        return render(request, 'login.html')
 
