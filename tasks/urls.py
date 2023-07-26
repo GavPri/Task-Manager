05e4097d134849a14e8fc5421e9209fae672299e
GavPri <gavinprior5@gmail.com>
2023-07-26 17:09:34 +0000

    url and view for adding tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_task_list, name='home')
+    path('', views.get_task_list, name='home'),
+    path('add-task', views.add_task, name='add')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -9,3 +9,11 @@ def get_task_list(request):
         'tasks': tasks
     }
     return render(request, 'tasks.html', context)
+
+
+def add_task(request):
+    tasks = Tasks.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'add-task.html')
