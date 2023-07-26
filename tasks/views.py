7ddd34f7db4917b6cb847bbdb541ab98e80522d2
GavPri <gavinprior5@gmail.com>
2023-07-26 17:49:56 +0000

    create form view

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tasks
 # Create your views here.
 
@@ -12,8 +12,16 @@ def get_task_list(request):
 
 
 def add_task(request):
-    tasks = Tasks.objects.all()
-    context = {
-        'tasks': tasks
-    }
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+        urgency = 'urgency' in request.POST
+        Tasks.objects.create(
+            title=title,
+            description=description,
+            due_date=due_date,
+            urgency=urgency)
+    
+        return redirect('home')
     return render(request, 'add-task.html')
