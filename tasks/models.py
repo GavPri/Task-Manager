7897d3b3eb8f2f226c8dd964ea75e292ba55412c
GavPri <gavinprior5@gmail.com>
2023-07-26 13:41:38 +0000

    str add to task model

--- tasks/models.py
+++ tasks/models.py
@@ -14,3 +14,6 @@ class Task(models.Model):
     due_date = models.DateTimeField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
     urgency = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
