1bd909aebccaff61e8838234efc6de6bc4648fc6
GavPri <gavinprior5@gmail.com>
2023-07-26 13:39:45 +0000

    start class model with choices and fields

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    # User Selection Of Status
+    STATUS_CHOICES = (
+        ('todo', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed')
+    )
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    due_date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    urgency = models.BooleanField(default=False)
