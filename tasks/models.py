f604f052bb4a9c2a3a05756399396bbf6722d0a1
GavPri <gavinprior5@gmail.com>
2023-07-31 14:02:43 +0000

    update model

--- tasks/models.py
+++ tasks/models.py
@@ -4,16 +4,10 @@ from django.db import models
 # Create your models here.
 class Tasks(models.Model):
     # User Selection Of Status
-    STATUS_CHOICES = (
-        ('todo', 'To Do'),
-        ('in_progress', 'In Progress'),
-        ('completed', 'Completed')
-    )
 
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
     due_date = models.DateTimeField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
     urgency = models.BooleanField(default=False)
 
     def __str__(self):
